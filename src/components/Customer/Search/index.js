d20d357160cbeb528861270b7f48534ea3841347
eriks junior <eriksjunior13@gmail.com>
2022-12-15 19:15:49 -0300

    refactor

--- src/components/Customer/Search/index.js
+++ src/components/Customer/Search/index.js
@@ -4,24 +4,22 @@ import { ClientContext } from "../../../context/Client/client"
 import { ContentSearch } from "./style"
 import { CardHeader } from "../../styles"
 
+import { AiOutlineSearch } from "react-icons/ai"
+
 export function SearchCliente({ children }) {
   const { searchClient, handleChangeSearchClient } = useContext(ClientContext)
 
   return (
     <div>
-      <CardHeader>
-        <ContentSearch className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-          <div className="inputSearch col-sm-7 col-md-7 col-lg-7 col-xl-7">
-            <input className="form-control form-control-sm" name="text" onChange={handleChangeSearchClient} placeholder="Pesquisar" />
-          </div>
+      <ContentSearch className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div className="inputSearch col-sm-7 col-md-7 col-lg-7 col-xl-7">
+          <input className="form-control form-control-sm" name="text" onChange={handleChangeSearchClient} placeholder="Pesquisar" />
+        </div>
 
-          <div className=" col-sm-1 col-md-1 col-lg-1 col-xl-1 d-flex">
-            <div>
-              <button type="button" className="btn btn-primary btn-sm" onClick={searchClient}>Pesquisar</button>
-            </div>
-          </div>
-        </ContentSearch>
-      </CardHeader>
+        <div className="icon" onClick={searchClient}>
+          <AiOutlineSearch size={23} color="white"/>
+        </div>
+      </ContentSearch>
 
       <div className="card">
         <div className="card-body">
--- src/components/Customer/Search/style.js
+++ src/components/Customer/Search/style.js
@@ -2,8 +2,20 @@ import styled from "styled-components";
 
 export const ContentSearch = styled.div`
   display: flex!important;
-  justify-content: space-evenly;
   gap: 15px;
+  margin-bottom: 10px;
+  margin-top: 30px;
+
+  .icon{
+    position: relative;
+    left: -45px;
+    background-color: #0070fc;
+    width: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+  }
   
 @media screen and (max-width: 575px) {
    .inputSearch{
