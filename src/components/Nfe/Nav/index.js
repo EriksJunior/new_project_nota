04e76d612aac68fe09497e514ff1fe14c0f429d4
eriks junior <eriksjunior13@gmail.com>
2022-12-23 18:48:22 -0300

    style(icons): adicionado todos icones kkk

--- src/components/Nfe/Nav/index.js
+++ src/components/Nfe/Nav/index.js
@@ -4,6 +4,10 @@ import { useContext } from "react"
 import { LeafContext } from "../../../context/Leaf/leaf"
 
 import { IoPersonAdd } from "react-icons/io5"
+import { FaTruckMoving } from "react-icons/fa"
+import { BsBasket2Fill } from "react-icons/bs"
+import { IoIosPaper } from "react-icons/io"
+
 
 export function Nav({ children }) {
   const { handleRenderNavItem } = useContext(LeafContext)
@@ -15,13 +19,13 @@ export function Nav({ children }) {
           <IoPersonAdd size={25} color={"white"} /><p>Cliente</p>
         </NavItems>
         <NavItems name="produto" onClick={(e) => handleRenderNavItem(e.target.name)}>
-          <IoPersonAdd size={25} color={"white"} /><p>Produto</p>
+          <BsBasket2Fill size={25} color={"white"} /><p>Produto</p>
         </NavItems>
         <NavItems name="transporte" onClick={(e) => handleRenderNavItem(e.target.name)}>
-          <IoPersonAdd size={25} color={"white"} /><p>Transporte</p>
+          <FaTruckMoving size={25} color={"white"} /><p>Transporte</p>
         </NavItems>
         <NavItems name="pedido" onClick={(e) => handleRenderNavItem(e.target.name)}>
-          <IoPersonAdd size={25} color={"white"} /><p>Pedido</p>
+          <IoIosPaper size={25} color={"white"} /><p>Pedido</p>
         </NavItems>
       </ContentNavItems>
 
