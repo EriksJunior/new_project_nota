6d9de4947213e2d7f7a80763468f74a70e5b329a
EriksJunior <EriksJunior13@gmail.com>
2022-12-28 00:59:22 -0300

    commit

--- src/components/Customer/Table/index.js
+++ src/components/Customer/Table/index.js
@@ -2,30 +2,30 @@ import { useContext } from "react"
 import { ClientContext } from "../../../context/Client/client"
 import { ContentTable } from "./style"
 
-import { IconEdit, IconDelete } from  "../../styles"
+import { IconEdit, IconDelete } from "../../styles"
 
 export function Table() {
   const { returnedClient, findById, deleteClient } = useContext(ClientContext)
   return (
-      <ContentTable className="table-responsive">
-        <table className="table table-dark table-striped">
-          <thead>
-            <tr>
-              <th >Nome</th>
-              <th>CPF/CNPJ</th>
-              <th>Telefone</th>
-              <th >Data Nascimento</th>
-              <th className="text-center">Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            {returnedClient.map((e) =>
-              <tr key={e.id} >
-                <td style={{color: "deepskyblue!important"}}>{e.nome}</td>
-                <td>{e.cpfCnpj}</td>
-                <td>{e.telefone}</td>
-                <td >{e.dataNascimento?.split("-").reverse().join("/")}</td>
-                <td>
+    <ContentTable className="table-responsive">
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th className="text-center">Nome</th>
+            <th className="text-center" >CPF/CNPJ</th>
+            <th className="text-center">Telefone</th>
+            <th className="text-center">Data Nascimento</th>
+            <th className="text-center">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {returnedClient.map((e) =>
+            <tr key={e.id} >
+              <td className="text-center name">{e.nome}</td>
+              <td className="text-center">{e.cpfCnpj}</td>
+              <td className="text-center">{e.telefone}</td>
+              <td className="text-center">{e.dataNascimento?.split("-").reverse().join("/")}</td>
+              <td>
                   <div className="d-flex justify-content-center gap-2">
                     <div onClick={() => findById(e.id)}>
                       <IconEdit cursor={"pointer"} size={20} />
@@ -35,10 +35,10 @@ export function Table() {
                     </div>
                   </div>
                 </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </ContentTable>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </ContentTable>
   )
 }
--- src/components/Product/Table/index.js
+++ src/components/Product/Table/index.js
@@ -4,39 +4,39 @@ import { ProductContext } from '../../../context/Product/product';
 import { IconEdit, IconDelete } from "../../styles"
 
 export function Table() {
-  const{ returnedProduct, findById, deleteProduct } = useContext(ProductContext)
+  const { returnedProduct, findById, deleteProduct } = useContext(ProductContext)
 
-  return(
-      <div className="table-responsive">
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Valor</th>
-              <th>Estoque</th>
-              <th className="text-center">Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            {returnedProduct.map((e) =>
-              <tr key={e.id} >
-                <td>{e.nome}</td>
-                <td>{e.valor.toLocaleString("pr-BR", {minimumFractionDigits: 2})}</td>
-                <td>{e.estoque}</td>
-                <td>
-                  <div className="d-flex justify-content-center gap-2">
-                    <div onClick={() => findById(e.id)}>
-                      <IconEdit cursor={"pointer"} size={20} />
-                    </div>
-                    <div onClick={() => deleteProduct(e.id)}>
-                      <IconDelete cursor={"pointer"} size={20} />
-                    </div>
+  return (
+    <div className="table-responsive">
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Valor</th>
+            <th>Estoque</th>
+            <th className="text-center">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {returnedProduct.map((e) =>
+            <tr key={e.id} >
+              <td>{e.nome}</td>
+              <td>{e.valor.toLocaleString("pr-BR", { minimumFractionDigits: 2 })}</td>
+              <td>{e.estoque}</td>
+              <td>
+                <div className="d-flex justify-content-center gap-2">
+                  <div onClick={() => findById(e.id)}>
+                    <IconEdit cursor={"pointer"} size={20} />
                   </div>
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+                  <div onClick={() => deleteProduct(e.id)}>
+                    <IconDelete cursor={"pointer"} size={20} />
+                  </div>
+                </div>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   )
 }
