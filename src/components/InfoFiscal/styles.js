afab83ac629225671cdd4c045105e79976609904
EriksJunior <EriksJunior13@gmail.com>
2023-01-12 21:34:22 -0300

    feat

--- src/components/InfoFiscal/styles.js
+++ src/components/InfoFiscal/styles.js
@@ -4,12 +4,20 @@ export const ContentInfoFiscal = styled.div`
   width: 60%;
   display: flex;
   flex-direction: column;
+  gap: 10px;
   padding: 10px;
   flex-wrap: wrap;
 
 
-  @media screen and (max-width: 1521px) {
+  @media screen and (max-width: 1579px) {
     width: 100%;
     flex-direction: column;
   }
+`
+
+export const CardInfoFiscal = styled.div`
+  width: 100%;
+  height: 250px;
+  border-radius: 5px;
+  padding: 10px;
 `
