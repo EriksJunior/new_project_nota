58c7c49c2e9c22ebdd3ea0ad380b0d9a60e4686f
EriksJunior <EriksJunior13@gmail.com>
2023-02-04 11:50:45 -0300

    refactor(customer): adicionado um common de cidade para o customer

--- src/components/Customer/index.js
+++ src/components/Customer/index.js
@@ -3,6 +3,8 @@ import { ClientContext } from "./context"
 
 import { Masks } from "../../utils/masks/Masks"
 
+import { cidades } from '../../common/customer';
+
 export function FormCustomer() {
   const { client, handleChange, clearAllInputs, handleSaveOrUpdate, switchBetweenComponents } = useContext(ClientContext)
   const { maskCep, maskCpfCnpj } = Masks()
@@ -58,33 +60,9 @@ export function FormCustomer() {
               <label className="form-label">Estado</label>
               <select className="form-select form-select-sm" onChange={handleChange} name="uf" value={client.uf || ""}>
                 <option>Selecione...</option>
-                <option value="AC">Acre</option>
-                <option value="AL">Alagoas</option>
-                <option value="AP">Amapá</option>
-                <option value="AM">Amazonas</option>
-                <option value="BA">Bahia</option>
-                <option value="CE">Ceará</option>
-                <option value="DF">Distrito Federal</option>
-                <option value="ES">Espirito Santo</option>
-                <option value="GO">Goiás</option>
-                <option value="MA">Maranhão</option>
-                <option value="MS">Mato Grosso do Sul</option>
-                <option value="MT">Mato Grosso</option>
-                <option value="MG">Minas Gerais</option>
-                <option value="PA">Pará</option>
-                <option value="PB">Paraíba</option>
-                <option value="PR">Paraná</option>
-                <option value="PE">Pernambuco</option>
-                <option value="PI">Piauí</option>
-                <option value="RJ">Rio de Janeiro</option>
-                <option value="RN">Rio Grande do Norte</option>
-                <option value="RS">Rio Grande do Sul</option>
-                <option value="RO">Rondônia</option>
-                <option value="RR">Roraima</option>
-                <option value="SC">Santa Catarina</option>
-                <option value="SP">São Paulo</option>
-                <option value="SE">Sergipe</option>
-                <option value="TO">Tocantins</option>
+                {cidades.map((cidade) => 
+                  <option key={cidade.value} value={cidade.value}>{cidade.nome}</option>
+                )}
               </select>
             </div>
 
