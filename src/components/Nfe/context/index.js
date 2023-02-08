370c116b83e21eeeec3cbb00edc6b07a2d6e43cd
eriks junior <eriksjunior13@gmail.com>
2023-02-08 13:07:16 -0300

    refactor: iniciado refatoração

--- src/components/Nfe/context/index.js
+++ src/components/Nfe/context/index.js
@@ -1,13 +1,34 @@
+// import { createContext } from "react";
+// import { UseLeaf } from "../hooks";
+
+// const LeafContext = createContext({})
+
+// function LeafProvider({ children }) {
+//   const { produtoLeaf, setProdutoLeaf, pedido, setPedido, handleChange, handleChangeProductLeaf, responseWebmania, returnedProductsLeaf, handleSaveOrUpdate, addProduct, deleteProduct, cpfCnpjCliente, handleTotalValueProducts, sendLeaf, handleShow, handleClose, show, searchLeaf, handleChangeSeachLeaf, search, resultSearchLeaf, findLeafById, deleteLeafAndProducts, handleTotalValueGeneralLeafInformation, cancelLeaf, handleCloseModalCancelLeaf, handleShowModalCancelLeaf, showModalCancelLeaf, handleChangeCancelLeaf, handleRenderNavItem, navItems, openLayouts, handleNewLeaf, switchBetweenComponents } = UseLeaf()
+
+//   return (
+//     <LeafContext.Provider value={{ produtoLeaf, setProdutoLeaf, pedido, setPedido, handleChange, handleChangeProductLeaf, responseWebmania, returnedProductsLeaf, handleSaveOrUpdate, addProduct, deleteProduct, cpfCnpjCliente, handleTotalValueProducts, sendLeaf, handleShow, handleClose, show, searchLeaf, handleChangeSeachLeaf, search, resultSearchLeaf, findLeafById, deleteLeafAndProducts, handleTotalValueGeneralLeafInformation, cancelLeaf, handleCloseModalCancelLeaf, handleShowModalCancelLeaf, showModalCancelLeaf, handleChangeCancelLeaf, handleRenderNavItem, navItems, openLayouts, handleNewLeaf, switchBetweenComponents }}>
+//       {children}
+//     </LeafContext.Provider>
+//   )
+// }
+
+
+// export { LeafProvider, LeafContext }
+
 import { createContext } from "react";
-import { UseLeaf } from "../hooks";
+import { UseGeneral } from "../hooks/UseGeneral";
 
 const LeafContext = createContext({})
 
 function LeafProvider({ children }) {
-  const { produtoLeaf, setProdutoLeaf, pedido, setPedido, handleChange, handleChangeProductLeaf, responseWebmania, returnedProductsLeaf, handleSaveOrUpdate, addProduct, deleteProduct, cpfCnpjCliente, handleTotalValueProducts, sendLeaf, handleShow, handleClose, show, searchLeaf, handleChangeSeachLeaf, search, resultSearchLeaf, findLeafById, deleteLeafAndProducts, handleTotalValueGeneralLeafInformation, cancelLeaf, handleCloseModalCancelLeaf, handleShowModalCancelLeaf, showModalCancelLeaf, handleChangeCancelLeaf, handleRenderNavItem, navItems, openLayouts, handleNewLeaf, switchBetweenComponents } = UseLeaf()
+  const { handleRenderNavItem, navItems, openLayouts, handleNewLeaf, switchBetweenComponents, handleOpenAreaLeaf } = UseGeneral()
 
   return (
-    <LeafContext.Provider value={{ produtoLeaf, setProdutoLeaf, pedido, setPedido, handleChange, handleChangeProductLeaf, responseWebmania, returnedProductsLeaf, handleSaveOrUpdate, addProduct, deleteProduct, cpfCnpjCliente, handleTotalValueProducts, sendLeaf, handleShow, handleClose, show, searchLeaf, handleChangeSeachLeaf, search, resultSearchLeaf, findLeafById, deleteLeafAndProducts, handleTotalValueGeneralLeafInformation, cancelLeaf, handleCloseModalCancelLeaf, handleShowModalCancelLeaf, showModalCancelLeaf, handleChangeCancelLeaf, handleRenderNavItem, navItems, openLayouts, handleNewLeaf, switchBetweenComponents }}>
+    <LeafContext.Provider value={{
+      handleRenderNavItem, navItems, openLayouts, handleNewLeaf, switchBetweenComponents, handleOpenAreaLeaf
+
+    }}>
       {children}
     </LeafContext.Provider>
   )
---
+++ src/components/Nfe/hooks/UseLeaf.js
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+import { UseCustomer } from "./UseCustomer"
+import { UseProduct } from "./UseProduct"
+
+export function UseLeaf() {
+
+  return {}
+}
