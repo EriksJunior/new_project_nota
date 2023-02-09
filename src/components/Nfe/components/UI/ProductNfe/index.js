457ccac7fa2ae0cfa12f633a9464eb801b31a3a6
eriks junior <eriksjunior13@gmail.com>
2023-02-08 22:51:17 -0300

    feat(leaf_products): pegando todas as informações dos inputs corretamente

--- src/components/Nfe/components/UI/ProductNfe/index.js
+++ src/components/Nfe/components/UI/ProductNfe/index.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react"
+import { useContext } from "react"
 import { LeafContext } from "../../../context"
 
 import { ContentHeaderTitle } from "../../../styles"
@@ -7,13 +7,7 @@ import { BsFillGearFill } from "react-icons/bs"
 import { ContentTable, InputTable, Table, InputSelect, TableHeader, TrBodyContent, TdWidth33, TdWidth16, TdWidth12, TdWidth10, TdWidthTotal, HeaderTdWidthTotal, TdActions, InvisibleHeaderTdActions, ContentButtonAddMoreItems } from "./styles"
 
 export function ProductNfe() {
-  const { productsFromSelectBox } = useContext(LeafContext)
-  const [products, setProducts] = useState([1])
-
-  const addProducts = () => {
-    const lastItem = products.length - 1
-    setProducts([...products, products[lastItem] + 1]);
-  }
+  const { productsFromSelectBox, addProducts, products, handleChangeProducts, saveLeafProducts } = useContext(LeafContext)
 
   return (
     <div className="card">
@@ -38,10 +32,10 @@ export function ProductNfe() {
             </thead>
 
             <tbody>
-              {products.map((e, i) =>
-                <TrBodyContent key={i}>
+              {products.map((item, index) =>
+                <TrBodyContent key={index}>
                   <TdWidth33>
-                    <InputSelect className="form-select form-select-product form-select-sm" name='idCliente'>
+                    <InputSelect className="form-select form-select-product form-select-sm" name='idProduto' value={products[index].idProduto} onChange={(e) => handleChangeProducts(e, index)}>
                       <option value="">---selecione---</option>
                       {productsFromSelectBox.map((product) =>
                         <option key={product.id} value={product.id}>{product.nome}</option>
@@ -50,24 +44,23 @@ export function ProductNfe() {
                   </TdWidth33>
 
                   <TdWidth16>
-                    <InputTable name="nome" type="text" className="form-control-sm" placeholder="UN" />
+                    <InputTable type="text" className="form-control-sm" placeholder="UN" name='unidade' value={products[index].unidade} onChange={(e) => handleChangeProducts(e, index)} />
                   </TdWidth16>
 
                   <TdWidth10>
-
-                    <InputTable name="nome" type="text" className="form-control-sm" placeholder="QNT" />
+                    <InputTable type="text" className="form-control-sm" placeholder="QNT" name='quantidade' value={products[index].quantidade} onChange={(e) => handleChangeProducts(e, index)} />
                   </TdWidth10>
 
                   <TdWidth12>
-                    <InputTable name="nome" type="text" className="form-control-sm" placeholder="0,0000" />
+                    <InputTable type="text" className="form-control-sm" placeholder="0,0000" name='subtotal' value={products[index].subtotal} onChange={(e) => handleChangeProducts(e, index)} />
                   </TdWidth12>
 
                   <TdWidth12>
-                    <InputTable name="nome" type="text" className="form-control-sm" placeholder="0,0000" />
+                    <InputTable type="text" className="form-control-sm" placeholder="0,0000" name='total' value={products[index].total} onChange={(e) => handleChangeProducts(e, index)} />
                   </TdWidth12>
 
                   <TdWidthTotal>
-                    <InputTable name="nome" type="text" className="form-control-sm" placeholder="0,0000" />
+                    <InputTable type="text" className="form-control-sm" placeholder="0,0000" name='desconto' value={products[index].desconto} onChange={(e) => handleChangeProducts(e, index)} />
                   </TdWidthTotal>
 
                   <TdActions role="button">
@@ -80,7 +73,7 @@ export function ProductNfe() {
 
           <ContentButtonAddMoreItems>
             <button className="btn btn-primary btn-sm" onClick={addProducts}>Adicionar Produtos</button>
-            <button className="btn btn-primary btn-sm">Salvar</button>
+            <button className="btn btn-primary btn-sm" onClick={saveLeafProducts}>Salvar</button>
           </ContentButtonAddMoreItems>
         </ContentTable>
       </div>
--- src/components/Nfe/hooks/UseProduct.js
+++ src/components/Nfe/hooks/UseProduct.js
@@ -1,13 +1,35 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import ProductServices from "../../../services/ProductService"
+import { INITIAL_VALUE_PRODUTOS } from "../initialStates"
 
 export function UseProduct() {
+  const [products, setProducts] = useState([INITIAL_VALUE_PRODUTOS])
   const [productsFromSelectBox, setProductsFromSelectBox] = useState([])
+
+  const handleChangeProducts = useCallback((e, index) => {
+    setProducts(prevProducts =>
+      prevProducts.map((product, i) => {
+        if (i === index) {
+          return { ...product, [e.target.name]: e.target.value };
+        }
+        return product;
+      })
+    );
+  }, []);
+
+  const addProducts = () => {
+    setProducts([...products, INITIAL_VALUE_PRODUTOS]);
+    console.log(products)
+  }
+
+  const saveLeafProducts = () => {
+    console.log(products)
+  }
 
   const getProcuctsFromSelectBox = async () => {
     const products = await ProductServices.getFromSelectBox()
     setProductsFromSelectBox(products)
   }
 
-  return { getProcuctsFromSelectBox, productsFromSelectBox }
+  return { getProcuctsFromSelectBox, productsFromSelectBox, addProducts, products, handleChangeProducts, saveLeafProducts }
 }
