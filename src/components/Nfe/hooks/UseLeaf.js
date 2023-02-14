48ba7f9a6d21e198a1aca1bd664bb981e0e192b6
eriks junior <eriksjunior13@gmail.com>
2023-02-14 08:58:39 -0300

    pequenas alterações para deixar salvo como processeguir

--- src/components/Nfe/hooks/UseLeaf.js
+++ src/components/Nfe/hooks/UseLeaf.js
@@ -1,15 +1,25 @@
 import { useState, useCallback } from "react"
 // import { UseCustomer } from "./UseCustomer";
-// import { UseProduct } from "./UseProduct"
+import { UseProduct } from "./UseProduct"
 
 import { INITIAL_VALUE_PEDIDO } from "../initialStates";
 
 export function UseLeaf() {
+  const { saveLeafProducts } = UseProduct()
   const [pedido, setPedido] = useState(INITIAL_VALUE_PEDIDO)
 
   const handleChangePedido = useCallback((e) => {
     setPedido({ ...pedido, [e.currentTarget.name]: e.currentTarget.value })
   }, [pedido])
+
+  const handleSave = async () => {
+    // await saveLeaf()
+    // await saveLeafProducts()
+  }
+
+  const saveLeaf = async () => {
+    //chamar serviço para salvar pedido
+  }
 
   return { pedido, handleChangePedido }
 }
