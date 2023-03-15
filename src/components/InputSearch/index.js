ebc316ac3b4b821b3b71023503f9fd92e39dfad6
EriksJunior <EriksJunior13@gmail.com>
2023-03-15 20:16:29 -0300

    input select finalizado, cambiarra ? talvez!

--- src/components/InputSearch/index.js
+++ src/components/InputSearch/index.js
@@ -1,34 +1,14 @@
-import { ContentInputSearch, Input, BorderInput, BorderButton, ButtonFilter, DropDownSelect, ContentTileAndIcon, ContentIcon, AnimateDropDown } from "./styles"
-import { BsArrowDownShort } from "react-icons/bs"
-export function InputSearch({ active = "notActive", title = "Filtro", data = ["Cliente", "CPF/CNPJ", "NF-e", "NFC-e"] }) {
+import { InputSelect } from "../InputSelect"
+import { ContentInputSearch, Input, BorderInput} from "./styles"
+
+export function InputSearch() {
   return (
     <ContentInputSearch>
       <BorderInput>
         <Input name="text" placeholder="Pesquisar" />
       </BorderInput>
 
-      <BorderButton>
-        <ButtonFilter placeholder="Filtro" className="isOpen">
-          <ContentTileAndIcon>
-            {title}
-            <ContentIcon>
-              <BsArrowDownShort size={25} color={"#a6a3a3"} className="animateIcon" />
-            </ContentIcon>
-          </ContentTileAndIcon>
-
-
-          <AnimateDropDown>
-            <DropDownSelect className={`${active} animateDrop`}>
-              {data.map((item, index) =>
-                <div key={index} style={{width: "100%"}} className="items">
-                  <span>{item}</span>
-                </div>
-              )}
-            </DropDownSelect>
-          </AnimateDropDown>
-
-        </ButtonFilter>
-      </BorderButton>
+    <InputSelect />
     </ContentInputSearch>
   )
 }
---
+++ src/components/InputSelect/styles.js
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+export const ButtonFilter = styled.button`
+  width: 150px;
+  height: 25px;
+  background-color: transparent;
+  color: #a6a3a3;
+  border-radius: 5px;
+  display: block;
+`
+
+export const DropDownSelect = styled.div`
+  width: 150px;
+  /* display: flex;
+  flex-direction: column; */
+  position: absolute;
+  left: 0;
+  top: 30px;
+  z-index: 9999;
+  background-color: white;
+  border-radius: 5px;
+  max-height: 150px;
+  overflow: auto;
+  color: black;
+  font-family: sans-serif;
+  font-weight: 600;
+  font-size: 13px;
+
+  .items {
+    width: 100%;
+    padding: 5px;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .items:hover {
+    background-color: #02769c;
+    color: white;
+    transition: ease-in 0.1s all;
+  }
+
+  ::-webkit-scrollbar-track {
+      background-color: transparent;
+  }
+  ::-webkit-scrollbar {
+      width: 3px;
+      background: #F4F4F4;
+      border-radius: 5px;
+  }
+  ::-webkit-scrollbar-thumb {
+      background: deepskyblue;
+      border-radius: 5px;
+  }
+`
+
+export const ContentIcon = styled.span`
+  color: white;
+  position: absolute;
+  right: 0;
+  
+  /* .animateIcon {
+  animation: rotateIcon 0.5s forwards;
+
+  @keyframes rotateIcon {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(180deg);
+    }
+  }
+} */
+`
+
+export const AnimateDropDown = styled.div`
+  .animateDrop {
+    animation-name: animationDropDown;
+    animation-duration: 0.3s;
+  }
+
+  @keyframes animationDropDown {
+    0% {
+      display: none;
+      opacity: 0;
+    }
+    100% {
+      display: block;
+      opacity: 1;
+    } 
+  }
+`
+
+export const ContentTileAndIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20px;
+` 
+
+export const BorderButton = styled.div`
+  position: relative;
+  border: solid 1px #afb4b6;
+  border-radius: 5px;
+`
